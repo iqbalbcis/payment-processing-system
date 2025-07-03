b6984e814b41b8f52341909dac416479b4d37d02
iqbalbcis <iqbalbcis@gmail.com>
2025-07-03 10:42:20 +0100

    user service updated

--- src/main/java/com/main/service/impl/UserServiceImpl.java
+++ src/main/java/com/main/service/impl/UserServiceImpl.java
@@ -16,7 +16,6 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    @Transactional
     @Override
     public User addUser(User user) {
         user.setAccountNumber(generateUniqueAccountNumber());
